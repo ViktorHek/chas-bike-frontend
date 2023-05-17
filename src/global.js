3eba8aa3be09e3b6297cd804e2d2bc322e0bab38
viktorHek <viktorkarlsson125@gmail.com>
2023-05-17 21:00:29 +0200

    adding testUser. bike WIP

---
+++ src/global.js
@@ -0,0 +1,10 @@
+const global = {
+  testUser: {
+    name: "Stricker Biker",
+    kilometers: 1337,
+    favoritBike: 2,
+    imgUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCaFa5S5I_ZGML8N7JSAa3EJsm7yU4r8w3Gw&usqp=CAU"
+  }
+}
+
+export default global
--- src/pages/Bike.page.jsx
+++ src/pages/Bike.page.jsx
@@ -1,11 +1,36 @@
+import global from "../global";
+import "../styles/bike.css";
 
 function Bike() {
+  const types = ["tour", "city", "hybrid", "Trailor"];
 
   return (
     <>
-      <h1>hello Bike</h1>
+      <div className="header-container">
+        <div className="inner-header-container">
+          <div className="header-text-container">
+            <h3>Hello {global.testUser.name} </h3>
+            <h1>Choose your bike</h1>
+          </div>
+          <div className="header-img-container">
+            <img src={global.testUser.imgUrl} alt="profilePic" />
+          </div>
+        </div>
+      </div>
+      <div className="type-selector-container">
+        <div className="inner-type-selector-container">
+          {types.map((el) => {
+            return <div className="chip">{el}</div>;
+          })}
+        </div>
+      </div>
+      <div className="bike-card-container">
+        <div className="inner-bike-card-container">
+          
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Bike
+export default Bike;
